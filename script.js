f84a65cd222837b291615457e3c16c8c8126b86e
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-31 19:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const audio = document.getElementById("audio");
+
+function play(src){
+	audio.src=src;
+	audio.play();
+}
+
+function stop(){
+	audio.pause();
+}
